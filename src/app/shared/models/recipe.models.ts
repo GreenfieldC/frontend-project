11d699ddcae8929bf59be9ab4a9b4a5d06c3ec41
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2025-03-13 20:45:21 +0100

    feat(Rezepte): Testliste mit Rezepten und dazugehörigen interface und enums erstellt
    Diese Testliste dient zur Demonstration der neuen Rezepte-Seite. Enthält mehrere Beispielrezepte.
    
    Closes #15

---
+++ src/app/shared/models/recipe.models.ts
@@ -0,0 +1,39 @@
+export interface Recipe {
+  title: string;
+  ingredients: Ingredient[];
+  personsNumber: number;
+  steps: string[];
+  recipeId?: number; // for routing
+  category: RecipeCategory[]; // several categories possible
+  dietType: DietType;
+  preparationTime: number; // in minutes
+}
+
+export interface Ingredient {
+  amount: number;
+  unit: string;
+  name: string;
+}
+
+export enum RecipeCategory {
+  Breakfast = 'Frühstück',
+  MainCourse = 'Hauptgericht',
+  SideDish = 'Beilage',
+  Dessert = 'Nachspeise',
+  Snack = 'Snack',
+  Beverage = 'Getränk',
+  Other = 'Sonstiges'
+}
+
+export enum DietType {
+  Omnivore = 'Vollkost',
+  Vegetarian = 'Vegetarisch',
+  Vegan = 'Vegan',
+  GlutenFree = 'Glutenfrei',
+  DairyFree = 'Laktosefrei',
+  NutFree = 'Nussfrei',
+  Paleo = 'Paleo',
+  Keto = 'Keto',
+  Halal = 'Halal',
+  Kosher = 'Koscher',
+}
