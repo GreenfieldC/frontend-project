2861ab52d864f06d4e00a46d6d8fbc046157ae76
TomG1909 <88586121+TomG1909@users.noreply.github.com>
2025-03-22 00:45:22 +0100

    cleaned up code

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,8 +1,6 @@
-import { DietType, Ingredient } from './../shared/models/recipe.models';
 import { Injectable } from '@angular/core';
 import { recipes } from '../shared/models/recipes';
 import { Recipe } from '../shared/models/recipe.models';
-import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,7 +10,12 @@ export class RecipeService {
   getAllRecipes() {
     return recipes;
   }
-  getRecipeById(id: number) {
-    return recipes.find((recipe) => recipe.recipeId == id);
+
+  getRecipeById(id: number): Recipe {
+    const recipe = recipes.find((recipe) => recipe.recipeId == id);
+    if (!recipe) {
+      throw new Error(`Recipe with id ${id} not found`);
+    }
+    return recipe;
   }
 }
