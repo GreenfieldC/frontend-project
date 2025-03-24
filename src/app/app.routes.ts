1d04bb6bf86d957ae5225e434dd77f0621d837ed
derFabio <fabio.virgolini@gmail.com>
2025-03-24 23:25:21 +0100

    17 reduces footer size

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,17 @@ import { HomeComponent } from './pages/home/home.component';
 import { RecipeDetailViewComponent } from './pages/recipe-detail-view/recipe-detail-view.component';
 import { RecipesOverviewComponent } from './pages/recipes-overview/recipes-overview.component';
 import { FavoriteOverviewComponent } from './pages/favorite-overview/favorite-overview.component';
+import {LayoutComponent} from './shared/components/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    component: HomeComponent,
-  },
-  {
-    path: 'recipes',
-    component: RecipesOverviewComponent,
-  },
-  {
-    path: 'recipes/:id',
-    component: RecipeDetailViewComponent,
-  },
-  {
-    path: 'favorites',
-    component: FavoriteOverviewComponent,
-  },
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'recipes', component: RecipesOverviewComponent },
+      { path: 'recipes/:id', component: RecipeDetailViewComponent },
+      { path: 'favorites', component: FavoriteOverviewComponent }
+    ]
+  }
 ];
